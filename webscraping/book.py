1fc7d1362b144370d5c24d43c8ca7b883cbbd440
Anna Klimiuk <ania.klimiuk411@gmail.com>
2020-07-07 13:10:08 +0200

    Add additional book informations

--- webscraping/book.py
+++ webscraping/book.py
@@ -2,8 +2,8 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 ##copy chromedriver into python folder
-driver = webdriver.Chrome('C:\ChromeDriver\chromedriver.exe')
 # driver = webdriver.Chrome('C:\ChromeDriver\chromedriver.exe')
+driver = webdriver.Chrome(r'C:\Users\aniak\chromedriver.exe')
 #driver.set_window_position(-2000,0)#this function will minimize the window
 first_url = 1
 last_url = 3    # Last book is 8,630,000
@@ -24,7 +24,21 @@ for book_reference_number in range(first_url, last_url):
         book_description = soup.select("#description span:nth-child(2)")[0].text.strip()
     except:
         book_description = ''
+    try:
+        book_format = soup.select("div.row span:nth-child(1)")[0].text.strip()
+    except:
+        book_format= ''
+    try:
+        book_language = soup.select("div[itemprop='inLanguage']")[0].text.strip()
+    except:
+        book_language= ''
+    try:
+        genre= [value.text.strip() for value in soup.select("div.elementList  div:nth-child(1)")]
+        genre_users= [value.text.strip() for value in soup.select("div.elementList  div:nth-child(2)")]
+    except:
+        genre = []
+        genre_users = []
 
 
-    print('NO.', book_reference_number, 'TITLE: ', book_title, 'AUTHOR: ', author_name,'DESCRIPTION: ',book_description) 
+    print('NO.', book_reference_number, ', TITLE: ', book_title, ', AUTHOR: ', author_name,', DESCRIPTION: ',book_description,', FORMAT: ', book_format,', LANGUAGE: ', book_language,', GENRES&USERS: ',genre,genre_users) 
    
